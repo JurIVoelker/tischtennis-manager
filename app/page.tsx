651e55d6c3f12ec4404541d64e2bd32f7e594677
JurIVoelker <Juri.Voelker@gmx.de>
2024-11-09 11:43:02 +0100

    feat: add user login component, skeleton UI, and mobile hook; update page and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import { prisma } from "@/lib/prisma";
+import { prisma } from "@/lib/prisma/prisma";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany();
+  const clubs = await prisma.club.findMany({
+    include: {
+      teams: true,
+    },
+  });
+  console.log(clubs);
   return (
     <div className="p-4 flex flex-col gap-y-4">
       <h2>Home</h2>
-
-      <ul className="flex flex-col gap-y-2">
-        {posts.map((post) => (
-          <li key={post.id}>{post.name}</li>
-        ))}
-      </ul>
+      {clubs.map((club) => (
+        <p>{club.name}</p>
+      ))}
     </div>
   );
 }
