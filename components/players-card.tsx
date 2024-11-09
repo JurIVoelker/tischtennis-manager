1f173454ccb07d17c428e203effa0a1eeb8816a6
JurIVoelker <Juri.Voelker@gmx.de>
2024-11-09 16:30:12 +0100

    feat: implement PlayersCard component with player badges and add Badge component for styling

--- components/players-card.tsx
+++ components/players-card.tsx
@@ -1,12 +1,19 @@
 import { Prisma } from "@prisma/client";
 import { Card } from "./ui/card";
+import { Badge } from "./ui/badge";
 
 interface PlayersCardProps {
   players: any; // TODO TYPE
 }
 
 const PlayersCard = ({ players }: PlayersCardProps) => {
-  return <Card className="p-6">{JSON.stringify(players)}</Card>;
+  return (
+    <Card className="p-6 flex flex-wrap gap-1">
+      {players.map((player) => (
+        <Badge variant="outline">{player.firstName}</Badge>
+      ))}
+    </Card>
+  );
 };
 
 export default PlayersCard;
