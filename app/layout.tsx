1277baa815656ede8fb4c612123f5781ba9b2ad5
JurIVoelker <Juri.Voelker@gmx.de>
2024-11-13 01:05:05 +0100

    feat: update global styles and integrate Inter font for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <div>
           <SidebarProvider>
