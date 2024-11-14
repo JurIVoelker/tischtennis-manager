2a6e0a81eedc2521bff943bb1de6b5c94d74ea51
JurIVoelker <Juri.Voelker@gmx.de>
2024-11-14 12:31:09 +0100

    fix: update regex patterns for login and team club pages to support query parameters refactor: enhance middleware logic for better URL handling and user token validation

--- middleware.ts
+++ middleware.ts
@@ -4,66 +4,93 @@ import { setTeamTokenCookie, teamCookieName } from "./lib/cookieUtils";
 import { handleUnauthorizedUser } from "./lib/middlewareUtils";
 import { getValidToken } from "./lib/APIUtils";
 import { MIDDLEWARE_STATUS_UNAUTHORIZED } from "./constants/middlewareConstants";
+import { LOGIN_PAGE_REGEX, TEAM_CLUB_PAGES_REGEX } from "./constants/regex";
 
 export async function middleware(request: NextRequest) {
   // Skip ignored routes
   const ignoredRoutes = ["ungueltiger-link"];
-  if (
-    ignoredRoutes.some((route) =>
-      new RegExp(`^https?://[^/]+/${route}`).test(request.url)
-    )
-  ) {
+  const urlPath = request.url.replace(/^https?:\/\/[^/]+/, "");
+
+  /*
+   * Skip ignored routes
+   */
+
+  if (ignoredRoutes.some((route) => urlPath.startsWith(`/${route}`))) {
     return NextResponse.next();
   }
 
-  // Get club and team slug
-  const clubSlug = "Test-Club";
-  const teamSlug = "Herren-I";
+  /*
+   * Get slugs from url
+   */
 
-  // Get user token from cookie
+  const splitUrl = urlPath.split("/");
+  const clubSlug = splitUrl[1];
+  const teamSlug = splitUrl[2];
+
+  /*
+   * Get valid token serverside
+   */
+
+  const token = await getValidToken(request, clubSlug, teamSlug);
+
+  if (token === null) {
+    return NextResponse.redirect(
+      new URL(
+        `/ungueltiger-link?statusCode=${MIDDLEWARE_STATUS_UNAUTHORIZED}`,
+        request.url
+      )
+    );
+  }
+
+  /*
+   * Check if user has token in cookies.
+   * If yes, validate it.
+   * If not, redirect to login page
+   */
+
+  const loginPageUrl = new URL(`/${clubSlug}/${teamSlug}/login`, request.url);
+
   const userToken = request.cookies.get(
     teamCookieName(clubSlug, teamSlug)
   )?.value;
 
-  /*
-   * Check if user token is valid
-   */
-  if (userToken) {
-    const token = await getValidToken(request, clubSlug, teamSlug);
-    if (token === null) {
-      return NextResponse.redirect(
-        new URL(
-          `/ungueltiger-link?statusCode=${MIDDLEWARE_STATUS_UNAUTHORIZED}`,
-          request.url
-        )
-      );
-    }
-    if (token === userToken) {
-      const response = NextResponse.next();
-      setTeamTokenCookie(response, clubSlug, teamSlug, token);
-      return response;
-    }
-  }
-
-  /*
-   * Handle unauthorized users
-   */
-  const authorizationStatus = await handleUnauthorizedUser(
-    request,
-    clubSlug,
-    teamSlug
-  );
-
-  if (typeof authorizationStatus === "string") {
-    // If the user could be authorized, by checking the invite token, redirect to the login page
-    const url = new URL(`/${clubSlug}/${teamSlug}/login`, request.url);
-    const response = NextResponse.redirect(url);
-    setTeamTokenCookie(response, clubSlug, teamSlug, authorizationStatus);
+  if (token === userToken) {
+    const response = NextResponse.next();
+    setTeamTokenCookie(response, clubSlug, teamSlug, token);
     return response;
-  } else if (authorizationStatus instanceof NextResponse) {
-    authorizationStatus.cookies.delete(teamCookieName(clubSlug, teamSlug));
-    return authorizationStatus;
+  } else if (!LOGIN_PAGE_REGEX.test(urlPath)) {
+    const response = NextResponse.redirect(loginPageUrl);
+    response.cookies.delete(teamCookieName(clubSlug, teamSlug));
+    return response;
   }
+
+  if (LOGIN_PAGE_REGEX.test(urlPath)) {
+    /*
+     * Handle unauthorized users
+     */
+    const authorizationStatus = await handleUnauthorizedUser(
+      request,
+      clubSlug,
+      teamSlug
+    );
+
+    if (typeof authorizationStatus === "string") {
+      // If the user could be authorized, by checking the invite token, redirect to the login page
+      const response = NextResponse.redirect(loginPageUrl);
+      setTeamTokenCookie(response, clubSlug, teamSlug, authorizationStatus);
+      return response;
+    } else if (authorizationStatus instanceof NextResponse) {
+      authorizationStatus.cookies.delete(teamCookieName(clubSlug, teamSlug));
+      return authorizationStatus;
+    }
+  }
+
+  return NextResponse.redirect(
+    new URL(
+      `/ungueltiger-link?statusCode=${MIDDLEWARE_STATUS_UNAUTHORIZED}`,
+      request.url
+    )
+  );
 }
 
 // See "Matching Paths" below to learn more
