5be0fba040cb90ae3f8ef7e21e3fb247a9dbadd4
Juri Völker <84136756+JurIVoelker@users.noreply.github.com>
2024-11-19 18:01:55 +0100

    Temporarily disable copy function

--- lib/stringUtils.ts
+++ lib/stringUtils.ts
@@ -3,9 +3,10 @@ import { getDateAndTime } from "./dateUtils";
 
 export const getInfoTextString = (match: Match) => {
   const { dateString, timeString } = getDateAndTime(match.matchDateTime);
-  return `Am Freitag, den ${dateString} um ${timeString} Uhr findet das Spiel gegen ${
+  /*return `Am Freitag, den ${dateString} um ${timeString} Uhr findet das Spiel gegen ${
     match.enemyClubName
   } statt. Wir spielen mit folgender Aufstellung: ${match.lineup
     .map((lineup) => lineup.player.firstName)
-    .join("\n - ")}`;
+    .join("\n - ")}`;*/
+  return "test";
 };
