88ac316ef16c64d59fe21417c6bf98ef4be056c9
Juri Völker <84136756+JurIVoelker@users.noreply.github.com>
2024-11-19 18:05:02 +0100

    Update stringUtils.ts

--- lib/stringUtils.ts
+++ lib/stringUtils.ts
@@ -1,8 +1,9 @@
 import { Match } from "@prisma/client";
-import { getDateAndTime } from "./dateUtils";
+//import { getDateAndTime } from "./dateUtils";
 
 export const getInfoTextString = (match: Match) => {
-  const { dateString, timeString } = getDateAndTime(match.matchDateTime);
+  console.log(match)
+  //const { dateString, timeString } = getDateAndTime(match.matchDateTime);
   /*return `Am Freitag, den ${dateString} um ${timeString} Uhr findet das Spiel gegen ${
     match.enemyClubName
   } statt. Wir spielen mit folgender Aufstellung: ${match.lineup
