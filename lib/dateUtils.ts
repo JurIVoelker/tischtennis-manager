f4d7954e8cdfe29fc32ea7fc65dabc3a97c9866d
JurIVoelker <Juri.Voelker@gmx.de>
2024-11-28 20:14:54 +0100

    fix: force german time

--- lib/dateUtils.ts
+++ lib/dateUtils.ts
@@ -1,5 +1,11 @@
 export const getDateAndTime = (dateTime: Date) => {
-  const dateString = `${dateTime.toLocaleDateString()}`;
-  const timeString = `${dateTime.getHours()}:${dateTime.getMinutes()}`;
+  const dateString = dateTime.toLocaleDateString("de-DE", {
+    timeZone: "Europe/Berlin",
+  });
+  const timeString = dateTime.toLocaleTimeString("de-DE", {
+    timeZone: "Europe/Berlin",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return { dateString, timeString };
 };
