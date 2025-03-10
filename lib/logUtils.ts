255df959a24ab014fa9ab60d111680fadfaefc64
JurIVoelker <Juri.Voelker@gmx.de>
2025-03-10 22:43:10 +0100

    feat: add auto import of games with script

--- lib/logUtils.ts
+++ lib/logUtils.ts
@@ -10,15 +10,19 @@ export type LogLevels =
   | "silly";
 
 export const asyncLog = (level: LogLevels, message: string) => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-  const isDev = process.env.NODE_ENV === "development";
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const isDev = process.env.NODE_ENV === "development";
 
-  axios
-    .post(`${baseUrl}/api/log`, {
-      status: level,
-      message: `${isDev ? "(dev) " : ""}${message}`,
-    })
-    .catch((error) => {
-      console.error("Failed to log message", error);
-    });
+    axios
+      .post(`${baseUrl}/api/log`, {
+        status: level,
+        message: `${isDev ? "(dev) " : ""}${message}`,
+      })
+      .catch((error) => {
+        console.error("Failed to log message", error);
+      });
+  } catch {
+    console.error(`logging failed: ${message}`);
+  }
 };
