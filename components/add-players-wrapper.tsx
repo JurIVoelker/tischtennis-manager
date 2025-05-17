d30fef487d552da239000c793332e8ab0d56078f
JurIVoelker <juri.voelker@gmx.de>
2025-05-17 11:55:18 +0200

    fix: adjust styling for player addition and lineup components

--- components/add-players-wrapper.tsx
+++ components/add-players-wrapper.tsx
@@ -109,7 +109,7 @@ const AddPlayersWrapper: React.FC<AddPlayersWrapperProps> = ({
           />
         </Card>
       )}
-      <div className="flex gap-2 w-full bottom-0 left-0 | bg-gradient-to-t from-white to-white/0 p-6 fixed | md:static md:p-0 | md:bg-transparent">
+      <div className="flex gap-2 w-full bottom-0 left-0 | bg-gradient-to-t from-background to-background/0 p-6 fixed | md:static md:p-0 | md:bg-transparent">
         <Link
           className={cn(buttonVariants({ variant: "outline" }), "w-full")}
           href={"./verwalten"}
