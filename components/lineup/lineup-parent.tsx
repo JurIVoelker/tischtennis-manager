2e09335a1cf50d382ce332dbb7793c039eb557a1
JurIVoelker <juri.voelker@gmx.de>
2026-01-08 20:02:06 +0100

    fix: track missing position priority for players in lineup

--- components/lineup/lineup-parent.tsx
+++ components/lineup/lineup-parent.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/prisma/prisma";
 import LineupChild from "./lineup-child";
 import { getTeamBaseName } from "@/lib/teamUtils";
 import { LineupWithPlayers } from "@/types/prismaTypes";
+import { umami } from "@/lib/umami";
 
 interface LineupProps {
   matchId: string;
@@ -18,7 +19,17 @@ const Lineup: React.FC<LineupProps> = async ({ matchId, teamSlug }) => {
   const baseName = getTeamBaseName(teamSlug.replaceAll("-", " "));
 
   const getPriority = (lineup: LineupWithPlayers) => {
-    return lineup?.player?.positionPriority?.[baseName] || 999;
+    // @ts-expect-error positionPriority is JSON field
+    const priority = lineup?.player?.positionPriority?.[baseName] || 999;
+    if (priority === 999) {
+      umami()?.track("missing-position-priority", {
+        playerId: lineup.player.id,
+        playerName: `${lineup.player.firstName} ${lineup.player.lastName}`,
+        teamBaseName: baseName,
+        teamSlug,
+      });
+    }
+    return priority;
   };
 
   const orderedLineup = lineups.sort((a, b) => getPriority(a) - getPriority(b));
